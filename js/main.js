e0a30d323e3f6196af0413ab4c8e0dbb8ca21d8a
mysunlight86 <45048751+mysunlight86@users.noreply.github.com>
2020-03-27 12:49:24 +0300

    I wrote a script to switch between tabs

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    // console.log('Страница загрузилась');
+    let tabsItem = $('.tabs-item');
+    // console.log(tabsItem);
+
+    tabsItem.on('click', function (event) {
+        event.preventDefault();
+        // console.log('Клик по табу');
+        // console.log($(this).attr('href'));
+        let activeContent = $(this).attr('href');
+        $('.visible').toggleClass('visible');
+        $(activeContent).toggleClass('visible');
+        $('tabs-item-active').toggleClass('tabs-item-active');
+        $(this).toggleClass('tabs-item-active');
+    });
+});
